1c1cd8d31abc330c5afae4ad9bc65ddc6b2943e4
EntenKoeniq <81123713+EntenKoeniq@users.noreply.github.com>
2022-04-21 18:08:38 +0200

    Added Promise() and small changes

--- test/test.js
+++ test/test.js
@@ -1,34 +1,29 @@
-var os = require('../lib/OSUtils');
+const os = require('../lib/OSUtils');
 
 console.log('\n');
-console.log( 'OS Utils');
+console.log('OS Utils');
 console.log('\n');
 
-console.log( 'Platform: ' + os.platform() );
-console.log( 'CPUs: ' + os.cpuCount() );
+console.log(`Platform: ${os.platform()}`);
+console.log(`CPUs: ${os.cpuCount()}`);
 console.log('\n');
 
-console.log( 'System Uptime (s): ' + os.sysUptime() );
-console.log( 'Process Uptime (s): ' + os.processUptime() );
+console.log(`System Uptime (s): ${os.systemUptime()}`);
+console.log(`Process Uptime (s): ${os.processUptime()}`);
 console.log('\n');
 
-console.log( 'Free Memory (Kb): ' + os.freemem() );
-console.log( 'total Memory (Kb): ' + os.totalmem() );
-console.log( 'Free Memory (%): ' + os.freememPercentage() );
+console.log(`Free Memory (Kb): ${os.freeMemory()}`);
+console.log(`total Memory (Kb): ${os.totalMemory()}`);
+console.log(`Free Memory (%): ${os.freeMemoryPercentage()}`);
 console.log('\n');
 
-console.log( 'Load Usage (%): ' + os.loadavg() );
-console.log( 'Load Usage 1 (%): ' + os.loadavg(1) );
-console.log( 'Load Usage 5 (%): ' + os.loadavg(5) );
-console.log( 'Load Usage 15 (%): ' + os.loadavg(15) );
+console.log(`Load Usage (%): ${os.loadAverage()}`);
+console.log(`Load Usage 1 (%): ${os.loadAverage(1)}`);
+console.log(`Load Usage 5 (%): ${os.loadAverage(5)}`);
+console.log(`Load Usage 15 (%): ${os.loadAverage(15)}`);
 console.log('\n');
 
-os.cpuUsage(function(v){
-	console.log( 'CPU Usage (%): ' + v );
-});
-
-os.cpuFree(function(v){
-	console.log( 'CPU Free (%): ' + v );
-});
+os.cpuUsage().then((v) => console.log(`CPU Usage (%): ${v}`));
+os.freeCpuUsage().then((v) => console.log(`CPU Free (%): ${v}`));
 
 console.log('\n');
